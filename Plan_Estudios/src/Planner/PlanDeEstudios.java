d2fede878b40828adb19438ce70a7b2146845ad9
DavidSaenz <jdsccr2@hotmail.com>
2021-04-09 08:52:08 -0500

    asf

--- Plan_Estudios/src/Planner/PlanDeEstudios.java
+++ Plan_Estudios/src/Planner/PlanDeEstudios.java
@@ -1,7 +1,7 @@
 package Planner;
 
 import java.util.*;
-
+import java.io.*; 
 
 public class PlanDeEstudios {
 
@@ -12,7 +12,7 @@ public class PlanDeEstudios {
 
 	String planSugerido;
 	String requisitos;
-
+	File source = new File ("docs/data cursos.txt");
 
 	//-------------------------------------------------------------
 	//methods
@@ -25,5 +25,38 @@ public class PlanDeEstudios {
 	public void cargarInfoBanner() {
 
 	}
+
+	public void escaneardoc() throws FileNotFoundException {
+
+		try {
+			Scanner fScn = new Scanner(source);
+			String data;
+
+			while( fScn.hasNextLine() ){
+				data = fScn.nextLine();
+
+				String[] token = data.split(";");
+				// NUM_BUZONES_P = Integer.parseInt(token[0]);
+				System.out.println(token[1]);
+
+			}
+			fScn.close(); 
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args){         
+		PlanDeEstudios consola = new PlanDeEstudios();   
+		try{
+			consola.escaneardoc();   
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	} 
+
+
 
 }
